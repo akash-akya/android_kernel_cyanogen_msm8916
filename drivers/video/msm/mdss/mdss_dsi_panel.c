725d0f9b9b68c7a7a3598b061047c154d9058403
akash akya <akashh246@gmail.com>
2016-03-25 09:48:14 +0530

    powersuspended: updated to v1.5

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -25,7 +25,11 @@
 #include "mdss_dsi.h"
 #include "mdss_livedisplay.h"
 
-#define DT_CMD_HDR 6
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
+ #define DT_CMD_HDR 6
 
 #ifdef CONFIG_MACH_LONGCHEER
 #define LCM_SUPPORT_READ_VERSION
@@ -615,7 +619,11 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
-	pinfo = &pdata->panel_info;
+#ifdef CONFIG_POWERSUSPEND
+    set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
+ 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
@@ -701,7 +709,11 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	if (ctrl->off_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds);
 
-end:
+#ifdef CONFIG_POWERSUSPEND
+    set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
+
+ end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_BLANK;
 	pr_debug("%s:-\n", __func__);
 	return 0;
