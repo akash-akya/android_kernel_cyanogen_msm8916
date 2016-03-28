28ce41a9d36ab3ae000a1cfded66ec5a32c0afcc
akash akya <akashh246@gmail.com>
2016-03-28 12:15:41 +0530

    cpufreq: add cpufreq governor intellidemand (version 5.0)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -440,6 +440,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 extern struct cpufreq_governor cpufreq_gov_cafactive;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_cafactive)
 #endif
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_intelliactive)
 
 /*********************************************************************
  *                     FREQUENCY TABLE HELPERS                       *
